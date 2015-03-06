e725970f119b39c6e43ac60501c33ccfeab325bf
YANG, Shuh-Hua <shyang107@gmail.com>
2015-03-07 02:50:19 +0800

    Initial Commit

---
+++ Factorization/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Factorization
+//
+//  Created by 楊樹華 on 2015/3/7.
+//  Copyright (c) 2015年 楊樹華. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
