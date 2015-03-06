a0087d922aa1f4529a60a262b84b1f714adf0fed
YANG, Shuh-Hua <shyang107@gmail.com>
2015-03-07 03:29:31 +0800

    create

---
+++ Factorization/dense_lu.c
@@ -0,0 +1,9 @@
+//
+//  dense_lu.c
+//  Factorization
+//
+//  Created by 楊樹華 on 2015/3/7.
+//  Copyright (c) 2015年 楊樹華. All rights reserved.
+//
+
+#include "dense_lu.h"
---
+++ Factorization/dense_lu.h
@@ -0,0 +1,14 @@
+//
+//  dense_lu.h
+//  Factorization
+//
+//  Created by 楊樹華 on 2015/3/7.
+//  Copyright (c) 2015年 楊樹華. All rights reserved.
+//
+
+#ifndef __Factorization__dense_lu__
+#define __Factorization__dense_lu__
+
+#include <stdio.h>
+
+#endif /* defined(__Factorization__dense_lu__) */
