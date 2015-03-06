83f34fe2bb7add3d5def98f6a936c9c76c73dd0b
YANG, Shuh-Hua <shyang107@gmail.com>
2015-03-07 03:02:36 +0800

    create

---
+++ Factorization/lu.h
@@ -0,0 +1,14 @@
+//
+//  lu.h
+//  Factorization
+//
+//  Created by 楊樹華 on 2015/3/7.
+//  Copyright (c) 2015年 楊樹華. All rights reserved.
+//
+
+#ifndef __Factorization__lu__
+#define __Factorization__lu__
+
+#include <stdio.h>
+
+#endif /* defined(__Factorization__lu__) */
